f3abe27728003239d7ce2c10b3745fd31d670443
Abhishek <jangal&mangal.gov>
2024-10-14 09:02:58 +0530

    Final Fix

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -1,46 +1,67 @@
-// app/api/posts/[id]/route.js
 import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
+const prisma = global.prisma || new PrismaClient();
+if (!global.prisma) {
+  global.prisma = prisma;
+}
 
 export async function GET(req, { params }) {
   const { id } = params;
 
-  const post = await prisma.post.findUnique({
-    where: { id: parseInt(id) },
-  });
+  try {
+    const post = await prisma.post.findUnique({
+      where: { id: parseInt(id) },
+    });
 
-  if (!post) {
-    return new Response("Post not found", { status: 404 });
+    if (!post) {
+      return NextResponse.json({ message: "Post not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(post, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching post:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
   }
-
-  return new Response(JSON.stringify(post), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
 }
 
 export async function PUT(req, { params }) {
   const { id } = params;
-  const body = await req.json();
 
-  const updatedPost = await prisma.post.update({
-    where: { id: parseInt(id) },
-    data: { ...body, date: new Date(body.date).toISOString() },
-  });
+  try {
+    const body = await req.json();
+    const updatedPost = await prisma.post.update({
+      where: { id: parseInt(id) },
+      data: { ...body, date: new Date(body.date).toISOString() },
+    });
 
-  return new Response(JSON.stringify(updatedPost), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+    return NextResponse.json(updatedPost, { status: 200 });
+  } catch (error) {
+    console.error("Error updating post:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
 
 export async function DELETE(req, { params }) {
   const { id } = params;
 
-  await prisma.post.delete({
-    where: { id: parseInt(id) },
-  });
+  try {
+    await prisma.post.delete({
+      where: { id: parseInt(id) },
+    });
 
-  return new Response(null, { status: 204 });
+    return NextResponse.json(null, { status: 204 });
+  } catch (error) {
+    console.error("Error deleting post:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
